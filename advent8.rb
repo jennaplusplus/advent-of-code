0804f1089ea4a664eb25f9a3b92e72d39b5dc605
Jenna Nichols <jennaplusplus@gmail.com>
2015-12-24 22:29:41 -0800

    day 8

--- advent8.rb
+++ advent8.rb
@@ -1,1 +1,32 @@
+string_count = 0
+code_count = 0
 
+File.open('advent8.txt', 'r').each do |line|
+  line = line.chomp[1..-2]
+
+  if line.match(/(\\\\)/)
+    slashes = line.scan(/(\\\\)/).length
+    line.gsub!("\\\\", "??")
+  else
+    slashes = 0
+  end
+
+  if line.match(/(\\x)/)
+    hexes = line.scan(/(\\x)/).length
+    line.gsub!("\\x", "**")
+  else
+    hexes = 0
+  end
+
+  if line.match(/(\\")/)
+    quotes = line.scan(/(\\")/).length
+    line.gsub!("\\\"", "!!")
+  else
+    quotes = 0
+  end
+
+  string_count += line.length - (3 * hexes) - quotes - slashes
+  code_count += line.inspect.length
+end
+
+puts "#{code_count} - #{string_count} = #{code_count - string_count}"
